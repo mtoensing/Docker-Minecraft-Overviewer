9873dd4d5222c9508d02c2421ee66ced5e2a721d
Marc Tönsing <marc@marc.tv>
2018-10-31 22:54:27 +0100

    changed name

--- run_example.sh
+++ run_example.sh
@@ -4,4 +4,4 @@ docker run \
   -v /Users/mtoe/Documents/mcserver/:/tmp/world/:ro \
   -v /Users/mtoe/Documents/config/:/tmp/config/:ro \
   -v /Users/mtoe/Documents/export/:/tmp/export/:rw \
-  -it c2c77a028467
+  -it overviewer113
