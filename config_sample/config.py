72536de7b312c5784dd1a87343f1043855e0af70
Marc Tönsing <marc@marc.tv>
2019-02-03 11:24:27 +0100

    added sample

---
+++ config_sample/config.py
@@ -0,0 +1,12 @@
+# My sample config.py script for viewer
+
+worlds["world"] = "/tmp/world/"
+texturepath = "/tmp/overviewer/client.jar"
+outputdir = "/tmp/export/"
+
+renders["day"] = {
+    'world': 'world',
+    'title': 'Day',
+    'rendermode': 'normal',
+    "dimension": "overworld",
+}
