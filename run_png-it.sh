3f527ab6aa84d80926e17d8251771ad8121192c7
Marc Tönsing <marc@marc.tv>
2019-10-13 14:20:50 +0200

    autocrop

--- run_png-it.sh
+++ run_png-it.sh
@@ -3,4 +3,4 @@ docker run \
   -v /Users/mtoe/Documents/mcserver/:/tmp/world/:ro \
   -v /Users/mtoe/Documents/config_test/:/tmp/config/:ro \
   -v /Users/mtoe/Documents/export/:/tmp/export/:rw \
-  -it --entrypoint "/usr/bin/python3" marctv/minecraft-overviewer /tmp/overviewer/contrib/png-it.py --memory-limit 3000 --zoom-level 6 --output /tmp/export/full.png /tmp/export/day/
+  -it --entrypoint "/usr/bin/python3" marctv/minecraft-overviewer /tmp/overviewer/contrib/png-it.py --memory-limit 3000 --zoom-level 6 --autocrop --output /tmp/export/full.png /tmp/export/day/
