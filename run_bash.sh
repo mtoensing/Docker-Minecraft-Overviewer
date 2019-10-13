05d3a9f0b9faa2c412d0a738e5a05110ecd05722
Marc Tönsing <marc@marc.tv>
2019-10-13 13:30:55 +0200

    Added scripts for PNG export and bash

---
+++ run_bash.sh
@@ -0,0 +1,6 @@
+docker run \
+  --rm \
+  -v /Users/mtoe/Documents/mcserver/:/tmp/world/:ro \
+  -v /Users/mtoe/Documents/config_test/:/tmp/config/:ro \
+  -v /Users/mtoe/Documents/export/:/tmp/export/:rw \
+  --entrypoint "/bin/bash" marctv/minecraft-overviewer -c /usr/bin/python3 ./tmp/overviewer/contrib/png-it.py --memory-limit 3000 --zoom-level 6 --output /tmp/export/full.png /tmp/export/day/
